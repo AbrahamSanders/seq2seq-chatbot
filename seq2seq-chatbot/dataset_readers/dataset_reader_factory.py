65e96890e392bd18da33a8681ff751b9cb4fe980
Avi Sanders <abraham.sanders@gmail.com>
2018-11-16 13:14:52 -0500

    Registered CSVDatasetReader in dataset_reader_factory.py

--- seq2seq-chatbot/dataset_readers/dataset_reader_factory.py
+++ seq2seq-chatbot/dataset_readers/dataset_reader_factory.py
@@ -3,6 +3,7 @@ Dataset reader implementation factory
 """
 from os import path
 from dataset_readers.cornell_dataset_reader import CornellDatasetReader
+from dataset_readers.csv_dataset_reader import CSVDatasetReader
 
 def get_dataset_reader(dataset_dir):
     """Gets the appropriate reader implementation for the specified dataset name.
@@ -13,7 +14,7 @@ def get_dataset_reader(dataset_dir):
     dataset_name = path.basename(dataset_dir)
 
     #When adding support for new datasets, add an instance of their reader class to the reader array below.
-    readers = [CornellDatasetReader()]
+    readers = [CornellDatasetReader(), CSVDatasetReader()]
 
     for reader in readers:
         if reader.dataset_name == dataset_name:
