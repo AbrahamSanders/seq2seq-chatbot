bd66936f6ca1e7dc3a2b8da02bba8e03ed62389f
Avi Sanders <abraham.sanders@gmail.com>
2018-11-16 13:32:51 -0500

    Added csv sample file and training batch files

---
+++ seq2seq-chatbot/datasets/csv/train_with_dependency_based_embeddings.bat
@@ -0,0 +1,5 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+cd ..\..
+python train.py --datasetdir=datasets\csv --encoderembeddingsdir=embeddings\dependency_based --decoderembeddingsdir=embeddings\dependency_based
+
+cmd /k
